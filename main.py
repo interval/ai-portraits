0a6b97e9e9593ea0099be1d6a8b2e74e00517a81
Ryan Coppolo <r@coppolo.com>
2023-02-13 14:08:48 -0500

    Update with latest tutorial

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 import os
 import glob
 import base64
-from datetime import datetime
-import io as PythonIO
+import io as _io
 import subprocess
-
-from interval_sdk import Interval, IO, action_ctx_var, io_var
+from datetime import datetime
+from interval_sdk import Interval, IO, ctx_var
 from interval_sdk.classes.page import Page
 from interval_sdk.classes.layout import Layout
-
 from huggingface_hub import hf_hub_download
 from diffusers import StableDiffusionPipeline
 import torch
 
+training_images_dir = "./training_images"
+base_model_path = "./Dreambooth-Stable-Diffusion/model.ckpt"
+trained_models_dir = "./trained_models"
+outputs_dir = "./outputs"
+
+images = {}
+for image_path in glob.glob(f"{outputs_dir}/*.png"):
+    with open(image_path, "rb") as f:
+        data = base64.b64encode(f.read()).decode("utf-8")
+        images[image_path] = {
+            "path": image_path,
+            "url": f"data:image/jpeg;base64,{data}",
+        }
+
 interval = Interval(
-    os.environ.get("INTERVAL_API_KEY"),
+    os.environ.get("INTERVAL_KEY"),
 )
 
 page = Page(name="Images")
@@ -22,42 +34,47 @@ page = Page(name="Images")
 
 @page.handle
 async def handler(display: IO.Display):
-    images = []
-    for image_path in glob.glob("/workspace/outputs/*.png"):
-        with open(image_path, "rb") as f:
-            data = base64.b64encode(f.read()).decode("utf-8")
-            images.append(f"data:image/jpeg;base64,{data}")
-
     return Layout(
         title="Images",
         description="AI generated images that you've previously generated.",
         children=[
             display.grid(
                 "Latest images",
-                data=images,
+                data=[image for key, image in images.items()],
                 render_item=lambda x: {
+                    "menu": [
+                        {
+                            "label": "Delete",
+                            "route": "images/delete",
+                            "params": {"path": x["path"]},
+                        }
+                    ],
                     "image": {
-                        "url": x,
+                        "url": x["url"],
                         "aspectRatio": 1,
                     },
                 },
-                default_page_size=9,
+                default_page_size=8,
                 is_filterable=False,
             ),
         ],
     )
 
 
-@page.action(slug="generate", name="Generate image")
+@page.action(unlisted=True)
 async def generate(io: IO):
 
-    prompt = await io.input.text(
-        "What's the prompt?",
-        help_text='To activate your trained images include "triggerword person" in the prompt.',
+    models = [f.name for f in os.scandir(trained_models_dir) if f.is_dir()]
+
+    [prompt, model] = await io.group(
+        io.input.text(
+            "What's the prompt?",
+            help_text='To activate your trained images include "triggerword person" in the prompt.',
+        ),
+        io.select.single("Which model do you want to use?", options=models),
     )
 
-    ctx = action_ctx_var.get()
-    num_steps = 50
+    ctx = ctx_var.get()
     await ctx.loading.start(
         title="Generating image...",
         description="This may take a while on a CPU."
@@ -65,22 +82,27 @@ async def generate(io: IO):
         else "",
     )
 
-    pipe = StableDiffusionPipeline.from_pretrained(
-        "/workspace/trained_models/RyanCoppolo"
-    ).to("cuda" if torch.cuda.is_available() else "cpu")
+    pipe = StableDiffusionPipeline.from_pretrained(f"{trained_models_dir}/{model}").to(
+        "cuda" if torch.cuda.is_available() else "cpu"
+    )
     image = pipe(
         prompt=prompt,
         negative_prompt="multiple people, ugly, deformed, malformed limbs, low quality, blurry, naked, out of frame",
-        num_inference_steps=num_steps,
+        num_inference_steps=50,
     ).images[0]
 
     now = datetime.now()
-    image.save(
-        f"/workspace/outputs/{now.strftime('%Y-%m-%d_%H-%M-%S')}.png", format="PNG"
-    )
+    image_path = f"{outputs_dir}/{now.strftime('%Y-%m-%d_%H-%M-%S')}.png"
+    image.save(image_path, format="PNG")
 
-    img_bytes = PythonIO.BytesIO()
+    img_bytes = _io.BytesIO()
     image.save(img_bytes, format="PNG")
+
+    data = base64.b64encode(img_bytes.getvalue()).decode("utf-8")
+    images[image_path] = {
+        "path": image_path,
+        "url": f"data:image/jpeg;base64,{data}",
+    }
 
     await io.display.image(
         "Generated image",
@@ -91,97 +113,126 @@ async def generate(io: IO):
     return "All done!"
 
 
+@page.action(unlisted=True)
+async def delete(io: IO):
+    ctx = ctx_var.get()
+    path = ctx.params.get("path", None)
+    if not path:
+        return "No image path provided"
+
+    if not os.path.isfile(path):
+        return "No such image to delete"
+
+    confirmed = await io.confirm(f"Really delete?", help_text="This can't be undone")
+    if confirmed:
+        os.remove(path)
+        del images[path]
+
+    return f"{path} deleted!"
+
+
 interval.routes.add("images", page)
 
 
 @interval.action
 async def train_model(io: IO):
+    ctx = ctx_var.get()
 
-    [_, token] = await io.group(
-        io.display.markdown(
-            "We're going to train a custom Stable Diffusion model on your images! First we'll download a pre-trained model to build off of. We'll need one of [your huggingface tokens](https://huggingface.co/settings/tokens) to download the base model."
-        ),
-        io.input.text("Enter your huggingface access token"),
+    model_name = await io.input.text(
+        "What do you want to call your custom model?",
+        help_text='This will also be used as the "trigger word" for your trained model.',
+    ).validate(
+        lambda name: "Model name must be one word (no spaces)"
+        if name.strip().count(" ") > 0
+        else None
     )
 
-    ctx = action_ctx_var.get()
-    await ctx.loading.start(
-        title="Downloading model...",
+    images = (
+        await io.input.file(
+            "Upload images to train your model on",
+            help_text="10-20 images is best, ideally with a variety of poses, lighting, distance, but also generally as close as possible to the kind of images you're trying to generate.",
+        )
+        .multiple()
+        .validate(
+            lambda files: "You must provide an even number of files"
+            if len(files) % 2 != 0
+            else None
+        )
     )
 
-    model_path = hf_hub_download(
-        repo_id="runwayml/stable-diffusion-v1-5",
-        filename="v1-5-pruned-emaonly.ckpt",
-        force_download=True,
-        token=token,
+    for image in images:
+        filename = f"training_images/{model_name}/{image.name}"
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+        with open(filename, "wb") as f:
+            f.write(image.read())
+
+    if not os.path.isfile(base_model_path):
+        await ctx.loading.start("Downloading model to train...")
+
+        model_path = hf_hub_download(
+            repo_id="runwayml/stable-diffusion-v1-5",
+            filename="v1-5-pruned.ckpt",
+        )
+
+        proc = subprocess.Popen(["readlink", "-f", model_path], stdout=subprocess.PIPE)
+        real_path = proc.stdout.read()
+        subprocess.run(
+            [
+                "mv",
+                real_path.strip(),
+                base_model_path,
+            ]
+        )
+
+    confirmed = await io.confirm(
+        "Ready to train?", help_text="This could take a while..."
     )
+    if confirmed:
+        await ctx.loading.start(title="Training model on your images...")
 
-    proc = subprocess.Popen(["readlink", "-f", model_path], stdout=subprocess.PIPE)
-    real_path = proc.stdout.read()
-    subprocess.run(
-        [
-            "mv",
-            real_path.strip(),
-            "Dreambooth-Stable-Diffusion/stable-diffusion-v1-5.ckpt",
-        ]
-    )
+        subprocess.run(
+            [
+                "python",
+                "Dreambooth-Stable-Diffusion/main.py",
+                "--base",
+                "Dreambooth-Stable-Diffusion/configs/stable-diffusion/v1-finetune_unfrozen.yaml",
+                "-t",
+                "--actual_resume",
+                base_model_path,
+                "--reg_data_root",
+                "Dreambooth-Stable-Diffusion/regularization_images/person_ddim",
+                "-n",
+                model_name,
+                "--gpus",
+                "0,",
+                "--data_root",
+                f"{training_images_dir}/{model_name}",
+                "--max_training_steps",
+                "2000",
+                "--class_word",
+                "person",
+                "--token",
+                model_name,
+                "--no-test",
+            ]
+        )
 
-    [_, trigger_word] = await io.group(
-        io.display.markdown(
-            "Model downloaded! Now we'll have you upload images (as soon as we build the I/O method)"
-        ),
-        io.input.text(
-            "What trigger word do you want to train on?",
-            help_text="Better to avoid spaces",
-            placeholder="MyName",
-        ),
-    )
+        await ctx.loading.start(
+            title="Done training! Converting to diffusers format..."
+        )
 
-    await ctx.loading.start(title="Training model on your images...")
+        latest_model_dir = max(glob.glob(f"./logs/{model_name}*"), key=os.path.getmtime)
 
-    subprocess.run(
-        [
-            "python",
-            "Dreambooth-Stable-Diffusion/main.py",
-            "--base",
-            "Dreambooth-Stable-Diffusion/configs/stable-diffusion/v1-finetune_unfrozen.yaml",
-            "-t",
-            "--actual_resume",
-            "Dreambooth-Stable-Diffusion/stable-diffusion-v1-5.ckpt",
-            "--reg_data_root",
-            "Dreambooth-Stable-Diffusion/regularization_images/person_ddim",
-            "-n",
-            trigger_word,
-            "--gpus",
-            "0",
-            "--data_root",
-            "Dreambooth-Stable-Diffusion/training_images",
-            "--max_training_steps",
-            "2000",
-            "--class_word",
-            "person",
-            "--token",
-            trigger_word,
-            "--no-test",
-        ]
-    )
-
-    await ctx.loading.start(title="Done training! Converting to diffusers format...")
-
-    parent_dir = "/workspace/logs"
-    pattern = "training_images*"
-    latest_dir = max(glob.glob(os.path.join(parent_dir, pattern)), key=os.path.getmtime)
-
-    subprocess.run(
-        [
-            "python",
-            "diffusers/scripts/convert_stable_diffusion_checkpoint.py",
-            "--checkpoint_path",
-            f"{latest_dir}/checkpoints/last.ckpt",
-            "--dump_path",
-            f"/workspace/trained_models/{trigger_word}",
-        ]
-    )
+        subprocess.run(
+            [
+                "python",
+                "diffusers/scripts/convert_original_stable_diffusion_to_diffusers.py",
+                "--checkpoint_path",
+                f"{latest_model_dir}/checkpoints/last.ckpt",
+                "--dump_path",
+                f"{trained_models_dir}/{model_name}",
+            ]
+        )
 
     return "All done!"
 
--- setup.sh
+++ setup.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
-mkdir outputs
+mkdir training_images
 mkdir trained_models
+mkdir outputs
 
 git clone https://github.com/huggingface/diffusers
 
 git clone https://github.com/JoePenna/Dreambooth-Stable-Diffusion
 cd Dreambooth-Stable-Diffusion
-git clone https://github.com/djbielejeski/Stable-Diffusion-Regularization-Images-person_ddim.git regularization_images
-mkdir training_images
 
 pip install omegaconf
 pip install einops
@@ -16,15 +15,20 @@ pip install pytorch-lightning==1.6.5
 pip install test-tube
 pip install transformers
 pip install kornia
-pip install -e git+https://github.com/CompVis/taming-transformers.git@master#egg=taming-transformers
-pip install -e git+https://github.com/openai/CLIP.git@main#egg=clip
 pip install setuptools==59.5.0
 pip install pillow==9.0.1
 pip install torchmetrics==0.6.0
 pip install -e .
 pip install protobuf==3.20.1
 pip install gdown
-pip install -qq diffusers["training"]==0.3.0 transformers ftfy
+pip install -qq diffusers transformers ftfy
 pip install huggingface_hub
 pip install captionizer==1.0.1
 pip install safetensors
+
+git clone https://github.com/djbielejeski/Stable-Diffusion-Regularization-Images-person_ddim.git regularization_images
+cd ..
+
+pip install interval-sdk
+pip install -e git+https://github.com/CompVis/taming-transformers.git@master#egg=taming-transformers
+pip install -e git+https://github.com/openai/CLIP.git@main#egg=clip
